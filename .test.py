0ef41f4f5f0dd55210190456bdd3686226770541
amounier <andre.mounier@ens-paris-saclay.fr>
2026-06-10 16:52:11 +0200

    download dpe details in xml format via selenium

--- .test.py
+++ .test.py
@@ -9,9 +9,15 @@ Created on Tue Jun  9 09:58:40 2026
 import time
 import os
 from urllib.request import urlopen, Request
-import random as rd
 import json
 import jsondiff
+import subprocess
+
+from selenium import webdriver
+from selenium.common import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
 
 # def download_dpe_details_requests(dpe_id, force=False):
 #     """
@@ -54,6 +60,77 @@ import jsondiff
 #             output.write(content.read())
             
 #     return
+
+
+
+def download_dpe_details_selenium(dpe_id, force=False, timeout = 20):
+    destination_folder = os.path.join(os.getcwd(),'data', 'DPE', 'XML')
+    os.makedirs(destination_folder, exist_ok=True)
+    success = True
+    
+    if f'{dpe_id}.xml' not in os.listdir(destination_folder) or force:
+        success = False
+        
+        # téléchargement du fichier xml
+        options = webdriver.ChromeOptions() # https://peter.sh/experiments/cgeadhromium-command-line-switches/ 
+        # options.add_argument("--incognito")
+        driver = webdriver.Chrome(options=options)
+        driver.get("https://observatoire-dpe-audit.ademe.fr/accueil")
+        wait = WebDriverWait(driver,timeout)
+        
+        selector_cookies = '#tarteaucitronAllDenied2'
+        wait.until(lambda driver: driver.find_element(by=By.CSS_SELECTOR, value=selector_cookies))
+        driver.find_element(by=By.CSS_SELECTOR, value=selector_cookies).click()
+        
+        time.sleep(0.3)
+        
+        selector_dpe = '#bienvenue > div > div.actions-widgets > app-widget-trouver-dpe > div.search-dpe > form > app-input-text-clearable > div > input'
+        wait.until(lambda driver: driver.find_element(by=By.CSS_SELECTOR, value=selector_dpe))
+        driver.find_element(by=By.CSS_SELECTOR, value=selector_dpe).send_keys(dpe_id+Keys.ENTER)
+        
+        time.sleep(0.3)
+        
+        selector_download = '#dd-dl-detail-dpe'
+        wait.until(lambda driver: driver.find_element(by=By.CSS_SELECTOR, value=selector_download))
+        driver.find_element(by=By.CSS_SELECTOR, value=selector_download).click()
+        
+        time.sleep(0.3)
+        
+        selector_xml = 'body > app-root > main > app-page-public-detail-dpe > app-detail-dpe > app-panel > div.header > div.header-right > app-dropdown > div > div > button:nth-child(2)'
+        wait.until(lambda driver: driver.find_element(by=By.CSS_SELECTOR, value=selector_xml))
+        driver.find_element(by=By.CSS_SELECTOR, value=selector_xml).click()
+        
+        # récupération du nom du dossier téléchargement
+        result = subprocess.run(
+            ["xdg-user-dir", "DOWNLOAD"],
+            capture_output=True, text=True
+        )
+        download_folder = result.stdout.strip()
+        
+        time.sleep(1)
+        
+        if f'{dpe_id}.xml' in os.listdir(download_folder):
+            success = True
+            driver.quit()
+        
+        if not success:
+            selector_error = 'body > ngb-modal-window > div > div > app-modal-error > app-modal-base > div.modal-footer > app-action-link > button'
+            try: 
+                wait.until(lambda driver: driver.find_element(by=By.CSS_SELECTOR, value=selector_error))
+                driver.find_element(by=By.CSS_SELECTOR, value=selector_error).click()
+                driver.quit()
+            except (NoSuchElementException, TimeoutException):
+                if f'{dpe_id}.xml' in os.listdir(download_folder):
+                    success = True
+                driver.quit()
+        
+        # déplacement du fichier téléchargé
+        if success:
+            origin_path = os.path.join(download_folder,f'{dpe_id}.xml')
+            subprocess.call(f"mv {origin_path} {destination_folder}", shell=True)
+        
+    return success
+
 
 
 def download_dpe_details(dpe_id, force=False):
@@ -102,12 +179,16 @@ def main():
     tic = time.time()
     
     dpe_id1 = '2375E2162413S'
-    download_dpe_details(dpe_id1)
+    dpe_id1 = '2275E2882995J'
+    # download_dpe_details(dpe_id1)
     
-    dpe_id2 = '2375E2258099Y'
-    download_dpe_details(dpe_id2)
+    # dpe_id2 = '2375E2258099Y'
+    # dpe_id2 = '2275E2946357J'
+    # download_dpe_details(dpe_id2)
     
-    print(compare_dpe_data(dpe_id1, dpe_id2)) 
+    # print(compare_dpe_data(dpe_id2, dpe_id1)) 
+    
+    print('Réussite :', download_dpe_details_selenium(dpe_id1,force=False,timeout=20))
     
     
     tac = time.time()
